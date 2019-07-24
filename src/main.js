eaac938756cd7f3da2a586520b2d067c33954bd4
xDroni <32304210+xdroni@users.noreply.github.com>
2019-07-24 23:52:44 +0200

    list the active clients

--- src/main.js
+++ src/main.js
@@ -1,21 +1,32 @@
 const { TeamSpeakClient } = require("node-ts");
-const {getArgument} = require("./utils.js");
+const { getArgument } = require("./utils.js");
 
-async function main(host) {
+async function main(host, login, password) {
     console.log('test:', host);
-    
+    console.log('login:', login);
+    console.log('password:', password);
+
     const client = new TeamSpeakClient(host);
     try {
         await client.connect();
         await client.send("use", {sid: 1});
 
         await client.send("login", {
-                                    client_login_name: 'test',
-                                    client_login_password: 'test'
+                                    client_login_name: login,
+                                    client_login_password: password
         });
 
-        const me = client.send("whoami");
-        console.log(me);
+        // const me = client.send("whoami");
+        // me.then((result) => {
+        //     console.log(result);
+        // });
+        //
+        let hostinfo = await client.send("hostinfo");
+        console.log(hostinfo);
+
+        let clientlist = await client.send("clientlist");
+        console.log(clientlist);
+
 
     } catch(err) {
         console.error("An error occurred: ");
@@ -23,4 +34,4 @@ async function main(host) {
     }
 }
 
-main(getArgument('host'));
+main(getArgument('host'), getArgument('login'), getArgument('password'));

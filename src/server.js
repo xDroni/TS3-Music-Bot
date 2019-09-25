f27918d941bff24a298d920e476c9d6055b517b8
xDroni <mrdroonix@gmail.com>
2019-09-25 12:14:32 +0200

    React frontend init with simple api call

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const port = 3000;
+const cors = require('cors');
+const port = 9000;
 
 module.exports = class Server {
     constructor(playlist) {
@@ -8,8 +9,15 @@ module.exports = class Server {
     }
 
     run() {
-        this.app.get('/asd', (req, res) => res.send('asd'));
-        this.app.use('/', (req, res) => res.send(this.Playlist.getCurrent().toString()));
+        this.app.use(cors());
+        this.app.use('/current', (req, res) => {
+            const data = {
+                title: 'a',
+                clientName: 'b',
+                url: 'c',
+            };
+            res.send(data);
+        });
 
         this.app.listen(port, () => console.log(`Example app listening on port ${port}!`))
     }

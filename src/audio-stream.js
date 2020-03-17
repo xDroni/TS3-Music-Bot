b3d9a1e356d14f9b1b58790e486ef64bd84f03b0
xDroni <mrdroonix@gmail.com>
2020-03-17 20:44:22 +0100

    removed lame, trying to decode audio from ffmpeg

---
+++ src/audio-stream.js
@@ -0,0 +1,16 @@
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const config = require('config');
+
+if(config.ffmpegExecutablePath)
+  ffmpeg.setFfmpegPath(config.ffmpegExecutablePath);
+
+function stream(url) {
+  const video = ytdl(url);
+
+  return ffmpeg()
+      .input(video)
+      .addOption('-f s16le')
+}
+
+module.exports = stream;

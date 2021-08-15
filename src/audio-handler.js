9ebd8201d23e4614d94cda3e61aace208b6f3c0e
dx_droni <mrdroonix@gmail.com>
2021-08-15 06:48:59 +0200

    Normalized audio

--- src/audio-handler.js
+++ src/audio-handler.js
@@ -1,36 +1,49 @@
 const stream = require('./audio-stream');
-const speaker = require('speaker');
+const portAudio = require('naudiodon');
+
 
 class AudioHandler {
-    
-    /** @param {string} url
-     * @param {string} clientName
-     * @param {string} title
-     */
-    constructor(url, clientName, title) {
-        this.url = url;
-        this.clientName = clientName;
-        this.title = title
-    }
-    
-    /** @param {Function} onEnd */
-    play(onEnd) {
-        this.s = stream(this.url)
-            .pipe(new speaker());
-        
-        this.s.on('error',(e) => {
-            this.s.destroy();
-            onEnd(e)
-        });
-        this.s.on('finish',() => {
-            this.s.destroy();
-            onEnd()
-        });
-    }
 
-    finish() {
-        this.s.emit('finish');
-    }
+  /** @param {string} url
+   * @param {string} clientName
+   * @param {string} title
+   */
+  constructor(url, clientName, title) {
+    this.url = url;
+    this.clientName = clientName;
+    this.title = title;
+  }
+
+  /** @param {Function} onEnd */
+  play(onEnd) {
+    const ao = new portAudio.AudioIO({
+      outOptions: {
+        channelCount: 2,
+        sampleFormat: portAudio.SampleFormat24Bit,
+        sampleRate: 48000,
+        deviceId: -1, // Use -1 or omit the deviceId to select the default device
+        closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
+      }
+    })
+
+    ao.start();
+
+    this.s = stream(this.url)
+        .pipe(ao)
+
+    this.s.on('error', (e) => {
+      this.s.destroy();
+      onEnd(e);
+    });
+    this.s.on('finish', () => {
+      this.s.destroy();
+      onEnd();
+    });
+  }
+
+  finish() {
+    this.s.emit('finish');
+  }
 }
 
 module.exports = AudioHandler;
